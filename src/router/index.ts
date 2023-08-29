8a3d0a994b03cbe197c02ce09613ed7b7229b135
TTiip <2711430287@qq.com>
2023-08-29 21:11:01 +0800

    chore: 路由重定向

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ const routes = [
   { path: '/', component: () => import('~/pages/index') },
   { path: '/test1', component: () => import('~/pages/test1') },
   { path: '/test2', component: () => import('~/pages/test2') },
+  { path: '/:catchAll(.*)', redirect: '/404' },
+  { path: '/404', name: '404', component: () => import('~/pages/[...all]') },
 ]
 
 export default routes
