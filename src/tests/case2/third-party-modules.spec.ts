52a63927cfc694cdf4f9604f64f7933ffd6fba01
TTiip <2711430287@qq.com>
2023-09-08 15:33:55 +0800

    test: 程序的间接输入-第三方库&对象&class&常量

--- src/tests/case2/third-party-modules.spec.ts
+++ src/tests/case2/third-party-modules.spec.ts
@@ -5,7 +5,11 @@ import { doubleUserAge } from './third-party-modules'
 vi.mock('axios')
 
 test('第三方模块的处理 axios', async () => {
-  vi.mocked(axios).mockResolvedValue({
+  // vi.mocked(axios).mockResolvedValue({
+  //   name: 'cxr',
+  //   age: 2,
+  // })
+  vi.mocked(axios.get).mockResolvedValue({
     name: 'cxr',
     age: 2,
   })
