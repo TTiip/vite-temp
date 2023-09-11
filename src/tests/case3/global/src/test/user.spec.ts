57b03186bcd09a8b13f5b264d2f60a6577db1f16
TTiip <2711430287@qq.com>
2023-09-11 20:37:48 +0800

    test: 程序的间接输入-间接层处理技巧（fix）

--- src/tests/case3/global/src/test/user.spec.ts
+++ src/tests/case3/global/src/test/user.spec.ts
@@ -1,5 +1,12 @@
-import { afterEach, beforeEach, expect, it, vi } from 'vitest'
+import { afterEach, expect, it, vi } from 'vitest'
 import { doubleCustomer, doubleHeight, doubleUserAge } from './user'
+
+// 全局mock 写在 测试case中的 beforeEach 中意义不大，所以直接写到最外面顶层
+vi.mock('./window.ts', () => ({
+  customerFn: () => {
+    return 10000
+  },
+}))
 
 afterEach(() => {
   vi.unstubAllGlobals()
@@ -24,13 +31,6 @@ it('global second', () => {
 })
 
 it('global third', () => {
-  beforeEach(() => {
-    vi.mock('./window.ts', () => ({
-      customerFn: () => {
-        return 10000
-      },
-    }))
-  })
   const r = doubleCustomer()
   expect(r).toBe(20000)
 })
