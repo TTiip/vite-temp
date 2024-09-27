ba88cedc4da92f50bb0a5a331f9b5c1251f98b38
TTiip <2711430287@qq.com>
2024-09-27 12:12:16 +0800

    feat: 通过动态 meta 信息修改 网页 title

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -2,6 +2,8 @@ import type { Router } from 'vue-router'
 
 export default (_: any, { router }: { router: Router }) => {
   router.beforeEach(async to => {
-
+    const { meta = {} } = to
+    const { title } = meta
+    ;(document as any).title = title
   })
 }
